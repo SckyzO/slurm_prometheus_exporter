33d2cc64789783855e86495e9608b6b2a04a3795
Tom <sckyzo@gmail.com>
2025-12-23 18:44:51 +0100

    feat: reduce histogram buckets from 17 to 9 for better performance

--- internal/metrics/registry.go
+++ internal/metrics/registry.go
@@ -44,7 +44,7 @@ func NewRegistry(version, gitCommit, buildTime string) *Registry {
 		prometheus.HistogramOpts{
 			Name:    "slurm_exporter_scrape_duration_seconds",
 			Help:    "Duration of scrapes by the exporter",
-			Buckets: prometheus.DefBuckets,
+			Buckets: []float64{0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10},
 		},
 		[]string{"endpoint"},
 	)
